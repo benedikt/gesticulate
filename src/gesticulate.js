b6fb8037d493710fde660b40e7ef4d8f8b6499a5
benedikt <benedikt@synatic.net>
2011-02-13 19:03:48 +0100

    Added ability to simulate touches using the mouse

--- src/gesticulate.js
+++ src/gesticulate.js
@@ -2,33 +2,67 @@ var Gesticulate = Class.create();
 
 //= require "gesticulate/geometry"
 //= require "gesticulate/recognizer"
+//= require "gesticulate/touch"
 //= require "gesticulate/language"
 
-Object.extend(Gesticulate, {
-  __gestures: {},
+Object.extend(Gesticulate, function() {
+  var mouseDown = false;
 
-  setup: function() {
-    var events = ['touchstart', 'touchmove', 'touchend'];
-    for(var i = 0; i < events.length; i++) {
-      document.addEventListener(events[i], this, false);
+  return {
+    __gestures: {},
+
+    setup: function(options) {
+      var events = ['touchstart', 'touchmove', 'touchend'];
+
+      if(options) {
+        if(options['simulate']) {
+          events = events.concat('mousedown', 'mousemove', 'mouseup');
+        }
+      }
+
+      for(var i = 0; i < events.length; i++) {
+        document.addEventListener(events[i], this, false);
+      }
+    },
+
+    registerGesture: function(name, block) {
+      var gesture = new Gesticulate.Language.Gesture();
+      block.call(this, gesture);
+      this.__gestures[name] = gesture;
+    },
+
+    observe: function(element, name, handler) {
+
+    },
+
+    fire: function(element, gesture) {
+
+    },
+
+    handleEvent: function(event) {
+      var touches = null;
+
+      if(/^mouse(up|down|move)$/(event.type)) {
+        switch(event.type) {
+          case 'mouseup':
+            mouseDown = false;
+            break;
+          case 'mousedown':
+            mouseDown = true;
+          case 'mousemove':
+            if(mouseDown) touches = [new Gesticulate.Touch(event.pageX, event.pageY)];
+        }
+      } else if(/^touch(start|move|end)$/(event.type)) {
+        touches = [];
+        for(var i = 0; i < event.touches.length; i++) {
+          var touch = event.touches[i];
+          touches.push(new Gesticulate.Touch(touch.pageX, touch.pageY));
+        }
+      }
+
+      if(touches) {
+        console.info(touches);
+      }
     }
-  },
-
-  registerGesture: function(name, block) {
-    var gesture = new Gesticulate.Language.Gesture();
-    block.call(this, gesture);
-    this.__gestures[name] = gesture;
-  },
-
-  observe: function(element, name, handler) {
-
-  },
-
-  fire: function(element, gesture) {
-
-  },
-
-  handleEvent: function(event) {
-    console.info(event);
   }
-});
+}());
---
+++ src/gesticulate/touch.js
@@ -0,0 +1,8 @@
+Gesticulate.Touch = Class.create(function () {
+  return {
+    initialize: function(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+}());
