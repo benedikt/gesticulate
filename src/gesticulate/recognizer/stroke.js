b4e76cf357ac716459ac2d60249475851a1084aa
benedikt <benedikt@synatic.net>
2011-03-12 22:31:30 +0100

    Implemented rotation invariance for Strokes. Not very useful for multitouch gestures but it's a start.

--- src/gesticulate/recognizer/stroke.js
+++ src/gesticulate/recognizer/stroke.js
@@ -6,31 +6,32 @@ Gesticulate.Recognizer.Stroke = function (_template, _threshold, _options) {
 
   var options = Gesticulate.Util.extend({
     positionInvariant: true,
-    scaleInvariant: true
+    scaleInvariant: false,
+    rotationInvariant: false
   }, _options || {});
 
   var points = [],
       template = _template,
       threshold = _threshold || 0.3;
 
-  function distanceToTemplate() {
+  var distanceToTemplate = function () {
     var distance = 0,
         interpolated = template.interpolate(points.length);
     for(var i = 0; i < points.length; i++) {
       distance += points[i].distanceTo(interpolated[i]);
     }
     return distance / points.length;
-  }
+  };
 
-  function normalizePositions() {
+  var normalizePositions = function () {
     var boundingBox = Gesticulate.Geometry.boundingBox(points);
     for(var i = 0; i < points.length; i++) {
       points[i].x -= boundingBox.position.x;
       points[i].y -= boundingBox.position.y;
     }
-  }
+  };
 
-  function normalizeScale() {
+  var normalizeScale = function () {
     var boundingBox = Gesticulate.Geometry.boundingBox(points),
         scale = template.boundingBox().diagonale() / boundingBox.diagonale();
 
@@ -38,7 +39,39 @@ Gesticulate.Recognizer.Stroke = function (_template, _threshold, _options) {
       points[i].x *= scale;
       points[i].y *= scale;
     }
-  }
+  };
+
+  var optimalRotation = function () {
+    var original_points = [],
+        minimum = Infinity,
+        rotation = 0;
+
+    for(var i = 0; i < points.length; i++) {
+      original_points[i] = points[i];
+      points[i] = new Gesticulate.Geometry.Point(points[i].x, points[i].y);
+    }
+
+    for(var g = 0; g < 2 * Math.PI; g += 0.1) {
+      for(i = 0; i < points.length; i++) {
+        points[i].x = points[i].x * Math.cos(0.1) - points[i].y * Math.sin(0.1);
+        points[i].y = points[i].x * Math.sin(0.1) + points[i].y * Math.cos(0.1);
+      }
+
+      normalizePositions();
+
+      if(distanceToTemplate() < minimum) {
+        rotation = g;
+        minimum = distanceToTemplate();
+      }
+    }
+
+    for(i = 0; i < points.length; i++) {
+      points[i].x = original_points[i].x * Math.cos(rotation) - original_points[i].y * Math.sin(rotation);
+      points[i].y = original_points[i].x * Math.sin(rotation) + original_points[i].y * Math.cos(rotation);
+    }
+
+    normalizePositions();
+  };
 
   this.update = function(point) {
     points.push(point);
@@ -47,6 +80,7 @@ Gesticulate.Recognizer.Stroke = function (_template, _threshold, _options) {
   this.recognize = function() {
       if(options.positionInvariant) { normalizePositions(); }
       if(options.scaleInvariant) { normalizeScale(); }
+      if(options.rotationInvariant) { optimalRotation(); }
 
       return distanceToTemplate() < (threshold * template.boundingBox().diagonale());
   };
