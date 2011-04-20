78b37be53c54d83ec645fc8333a0a32bc3f8e6eb
benedikt <benedikt@synatic.net>
2011-04-20 15:54:47 +0200

    Implemented the StayRecognizerBuilder

---
+++ src/gesticulate/language/stay_recognizer_builder.js
@@ -0,0 +1,18 @@
+Gesticulate.Language.StayRecognizerBuilder = function() {
+  var x, y, tolerance;
+
+  this.near = this.at = function(_x, _y) {
+    x = _x;
+    y = _y;
+    return this;
+  };
+
+  this.withToleranceOf = function(_tolerance) {
+    tolerance = _tolerance;
+    return this;
+  };
+
+  this.build = function() {
+    return new Gesticulate.Recognizer.Stay(x, y, tolerance);
+  };
+};
