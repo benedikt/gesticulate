a6bdc5e3f64cf79d2d3027549208698eec9a7f41
benedikt <benedikt@synatic.net>
2011-04-20 15:55:27 +0200

    Add new files to generated output

--- src/gesticulate/language.js
+++ src/gesticulate/language.js
@@ -5,5 +5,9 @@ Gesticulate.Language = {};
 
 //= require "language/gesture_builder"
 //= require "language/recognizer_builder"
+//= require "language/stroke_recognizer_builder"
+//= require "language/stay_recognizer_builder"
 //= require "language/line_builder"
+//= require "language/rectangle_builder"
+//= require "language/circle_builder"
 //= require "language/letter_stroke_builder"
