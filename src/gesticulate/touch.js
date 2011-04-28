e7e83415fa7f3025cee79f4eb9402105402e8c58
benedikt <benedikt@synatic.net>
2011-04-28 15:23:21 +0200

    Added event type to touch

--- src/gesticulate/touch.js
+++ src/gesticulate/touch.js
@@ -1,7 +1,8 @@
 /**
  * class Gesticulate.Touch
  **/
-Gesticulate.Touch = function (x, y, identifier) {
+Gesticulate.Touch = function (x, y, identifier, event) {
   this.position = new Gesticulate.Geometry.Point(x, y);
   this.identifier = identifier;
+  this.event = event;
 };
