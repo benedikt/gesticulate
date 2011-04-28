67b73da4e7ab0e3da981221994edf7b8709fe1a5
benedikt <benedikt@synatic.net>
2011-04-28 15:22:20 +0200

    Fixed call to initializer in line recognizer

--- src/gesticulate/recognizer/line.js
+++ src/gesticulate/recognizer/line.js
@@ -1,9 +1,9 @@
 /**
  * class Gesticulate.Recognizer.Line < Gesticulate.Recognizer.Base
  **/
-Gesticulate.Recognizer.Line = function(from_x, from_y, to_x, to_y) {
+Gesticulate.Recognizer.Line = function(from_x, from_y, to_x, to_y, options) {
   var line = new Gesticulate.Geometry.Line(from_x, from_y, to_x, to_y);
-  Gesticulate.Recognizer.Stroke.apply(this, line);
+  Gesticulate.Recognizer.Stroke.call(this, line, options);
 };
 
 Gesticulate.Recognizer.Line.prototype = new Gesticulate.Recognizer.Stroke();
