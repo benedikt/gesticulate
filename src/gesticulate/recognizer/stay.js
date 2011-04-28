36075c6df97df49a871428d3863370fe03e73e08
benedikt <benedikt@synatic.net>
2011-04-28 15:21:46 +0200

    Stay recognizer doesn't need a point anymore

--- src/gesticulate/recognizer/stay.js
+++ src/gesticulate/recognizer/stay.js
@@ -1,11 +1,11 @@
 /**
  * class Gesticulate.Recognizer.Stay < Gesticulate.Recognizer.Base
  **/
-Gesticulate.Recognizer.Stay = function (x, y, tolerance) {
+Gesticulate.Recognizer.Stay = function (tolerance, x, y) {
   var point = new Gesticulate.Geometry.Point(x, y),
       polyline = new Gesticulate.Geometry.Polyline();
 
-  if(!tolerance) tolerance = 20;
+  if(!tolerance) tolerance = 30;
 
   this.update = function(point) {
     polyline.addPoint(point);
@@ -13,6 +13,14 @@ Gesticulate.Recognizer.Stay = function (x, y, tolerance) {
 
   this.recognize = function() {
     if(polyline.points.length < 1) return false;
+
+    if(!x || !y) {
+      var box = polyline.boundingBox();
+      if(box.width > tolerance || box.height > tolerance) {
+           return false;
+      }
+      point = box.center();
+    }
 
     for(var i = 0; i < polyline.points.length; i++) {
       var distance = polyline.points[i].distanceTo(point);
